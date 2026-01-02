1c41742b48dd64b1d632429f26266c5f9c89e56a
Isaiah Andrews <isaiah1@uchicago.edu>
2026-01-02 14:40:19 -0600

    Rename cartridge

---
+++ src/cartridge.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "byte.h"
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool cart_init(const char *rom_path);
+void cart_deinit(void);
+
+byte cart_read(uint16_t addr);
+void cart_write(uint16_t addr, byte val);
